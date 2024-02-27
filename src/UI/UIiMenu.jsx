6e5ffa638f37f48bb44ca221c979d6e5b731c1e8
m1rageLA <miragela.code@gmail.com>
2024-02-27 16:44:56 +0100

    Add react-toastify dialog window

--- src/UI/UIiMenu.jsx
+++ src/UI/UIiMenu.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import DialogForm from "../server/DialogForm";
 
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 const UiMenu = () => {
   const [text, setText] = useState("");
 
@@ -12,7 +15,7 @@ const UiMenu = () => {
   const onButtonClick = async () => {
     try {
       const data = await DialogForm(text);
-      console.log("success", data);
+      console.log(data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -22,6 +25,7 @@ const UiMenu = () => {
     <div className="main-ui">
       <input type="text" value={text} onChange={onTextChange} />
       <button onClick={onButtonClick}>Fetch Data</button>
+      <ToastContainer position="top-center" closeOnClick="true" />
     </div>
   );
 };
