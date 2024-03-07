d09392281dbab6ccb07ad8c31227e3e607db673d
Wojciech Męczyński <wojteq898@gmail.com>
2024-03-07 17:09:40 +0100

    Minor changes to UI scale

--- src/components/user-interface/Login.jsx
+++ src/components/user-interface/Login.jsx
@@ -15,11 +15,11 @@ const Login = () => {
   return (
     <div className="login-container"> {}
       <div className="login-content"> {}
-        <label htmlFor="login">Zaloguj się</label><br />
+        <label htmlFor="login">Zaloguj się</label>
         <label htmlFor="email-label">E-mail</label>
-        <input type="text" id="email" /><br /> <br/>
+        <input type="text" id="email" /><br /> 
         <label htmlFor="password-label">Hasło</label>
-        <input type="password" id="password" /><br /> <br/> <br/>
+        <input type="password" id="password" /><br/><br/>
         <button onClick={handleLogin} className="login-button">Zaloguj się</button><br /> <br/>
         <button onClick={handleRegister} className="register-button">Zarejestruj się</button><br />
       </div>
