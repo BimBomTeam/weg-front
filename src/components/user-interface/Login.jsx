672120333aea565eca8c5c0c23a6980d86b2b2c6
Bartosz Arseniuk <bartekarseniuk1701@gmail.com>
2024-03-09 15:43:56 +0100

    Replace getElementById with useState and fix CSS

--- src/components/user-interface/Login.jsx
+++ src/components/user-interface/Login.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
     if (!email || !password) {
       toast.error("Fields cannot be empty");
     } else {
-      navigate('/game');
+      navigate("/game");
     }
-    
   };
 
   const handleRegister = () => {
-    navigate('/register');
+    navigate("/register");
   };
 
   return (
-    <div className="login-container"> {}
-      <div className="login-content"> {}
+    <div className="login-container">
+      {" "}
+      {}
+      <div className="login-content">
+        {" "}
+        {}
         <label htmlFor="login">Zaloguj się</label>
         <label htmlFor="email-label">E-mail</label>
-        <input type="text" id="email" /><br /> 
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="password-label">Hasło</label>
-        <input type="password" id="password" /><br/><br/>
-        <button onClick={handleLogin} className="login-button">Zaloguj się</button><br /> <br/>
-        <button onClick={handleRegister} className="register-button">Zarejestruj się</button><br />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="login-buttonBlock">
+          <button onClick={handleLogin} className="login-button">
+            Zaloguj się
+          </button>
+          <button onClick={handleRegister} className="to-register-button">
+            Zarejestruj się
+          </button>
+        </div>
       </div>
       <ToastContainer position="top-center" closeOnClick={true} />
     </div>
--- src/components/user-interface/Register.jsx
+++ src/components/user-interface/Register.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
 
   const handleRegister = () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const repeatPassword = document.getElementById('repeat-password').value;
-    
     if (!email || !password || !repeatPassword) {
       toast.error("Fields cannot be empty");
     } else if (password !== repeatPassword) {
@@ -25,17 +24,34 @@ const Register = () => {
   };
 
   return (
-    <div className="register-container"> {}
-      <div className="register-content"> {}
+    <div className="register-container">
+      <div className="register-content">
         <button onClick={handleBack} className="back-button"></button>   
         <label htmlFor="register">Rejestracja</label>
         <label htmlFor="email-register-label">E-mail</label>
-        <input type="text" id="email" /><br />
+        <input 
+          type="text" 
+          id="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
         <label htmlFor="password-register-label">Hasło</label>
-        <input type="password" id="password" /><br/>
+        <input 
+          type="password" 
+          id="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
         <label htmlFor="password-repeat-label">Powtórz hasło</label>
-        <input type="password" id="repeat-password" /><br/><br/> <br />
-        <button onClick={handleRegister} className="register-button">Zarejestruj się</button><br />
+        <input 
+          type="password" 
+          id="repeat-password" 
+          value={repeatPassword} 
+          onChange={(e) => setRepeatPassword(e.target.value)} 
+        />
+        <button onClick={handleRegister} className="register-button">
+          Zarejestruj się
+        </button>
       </div>
       <ToastContainer position="top-center" closeOnClick={true} />
     </div>
