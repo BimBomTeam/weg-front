9c8e5e6f0e0be5112c5c4080f14171035e6d7871
m1rageLA <miragela.code@gmail.com>
2024-04-13 10:25:14 +0200

    Implement alpha adaptation (1800px)

--- src/components/interactive-game-ui/UIiMenu.jsx
+++ src/components/interactive-game-ui/UIiMenu.jsx
@@ -106,65 +106,73 @@ const UiMenu = () => {
     setIsListening(!isListening);
   };
 
+  function getStyles(name) {
+    return getComputedStyle(document.documentElement).getPropertyValue(name);
+  }
+
   const animationProps = useSpring({
     height: isExpandButtonClicked
       ? isButtonClicked
-        ? "250px"
-        : "850px"
+        ? `${getStyles("--animationProps_height_expanded_clicked_true")}`
+        : `${getStyles("--animationProps_height_expanded_unclicked_false")}`
       : isButtonClicked
-      ? "850px"
-      : "250px",
+      ? `${getStyles("--animationProps_height_unexpanded_clicked_false")}`
+      : `${getStyles("--animationProps_height_unexpanded_unclicked_true")}`,
     transform: isVisible
       ? isExpandButtonClicked
-        ? "translateY(0%)"
-        : "translateY(0%)"
-      : "translateY(100%)",
+        ? `${getStyles(
+            "--animationProps_transform_visible_expanded_clicked_true"
+          )}`
+        : `${getStyles(
+            "--animationProps_transform_visible_expanded_unclicked_false"
+          )}`
+      : `${getStyles("--animationProps_transform_invisible_true")}`,
     top: isExpandButtonClicked
       ? isButtonClicked
-        ? "70%"
-        : "8%"
+        ? `${getStyles("--animationProps_top_expanded_clicked_true")}`
+        : `${getStyles("--animationProps_top_expanded_unclicked_false")}`
       : isButtonClicked
-      ? "8%"
-      : "70%",
+      ? `${getStyles("--animationProps_top_unexpanded_clicked_false")}`
+      : `${getStyles("--animationProps_top_unexpanded_unclicked_true")}`,
   });
 
   const textareaAnimationProps = useSpring({
     marginTop:
       (isExpandButtonClicked && isButtonClicked) ||
       (!isExpandButtonClicked && !isButtonClicked)
-        ? "0px"
-        : "410px",
+        ? `${getStyles("--textareaAnimationProps_marginTop_true")}`
+        : `${getStyles("--textareaAnimationProps_marginTop_false")}`,
   });
 
   const buttonAnimationProps = useSpring({
     marginTop:
       (isExpandButtonClicked && isButtonClicked) ||
       (!isExpandButtonClicked && !isButtonClicked)
-        ? "0px"
-        : "210px",
+        ? `${getStyles("--buttonAnimationProps_marginTop_true")}`
+        : `${getStyles("--buttonAnimationProps_marginTop_false")}`,
   });
 
   const buttonVoiceProps = useSpring({
     marginTop:
       (isExpandButtonClicked && isButtonClicked) ||
       (!isExpandButtonClicked && !isButtonClicked)
-        ? "0px"
-        : "350px",
+        ? `${getStyles("--buttonVoiceProps_marginTop_true")}`
+        : `${getStyles("--buttonVoiceProps_marginTop_false")}`,
   });
 
   const isListeningProps = useSpring({
     marginTop:
       (isExpandButtonClicked && isButtonClicked) ||
       (!isExpandButtonClicked && !isButtonClicked)
-        ? "0px"
-        : "133px",
+        ? `${getStyles("--isListeningProps_marginTop_true")}`
+        : `${getStyles("--isListeningProps_marginTop_false")}`,
     width: isListening
       ? isButtonClicked
-        ? "45px"
-        : "45px"
+        ? `${getStyles("--isListeningProps_width_true_true")}`
+        : `${getStyles("--isListeningProps_width_true_false")}`
       : isListening
-      ? "45px"
-      : "0px",
+      ? `${getStyles("--isListeningProps_width_false_true")}`
+      : `${getStyles("--isListeningProps_width_false_false")}`,
   });
 
   const buttonExpandProps = useSpring({
--- src/components/user-interface/user-interface/UserInterface.jsx
+++ src/components/user-interface/user-interface/UserInterface.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useSpring, animated } from "react-spring";
-import AccountModal from './modals/AccountModal';
-import DifficultyModal from './modals/DifficultyModal';
-import ShopModal from './modals/ShopModal';
-import InstructionModal from './modals/InstructionModal';
-import SettingsModal from './modals/SettingsModal';
-
+import AccountModal from "./modals/AccountModal";
+import DifficultyModal from "./modals/DifficultyModal";
+import ShopModal from "./modals/ShopModal";
+import InstructionModal from "./modals/InstructionModal";
+import SettingsModal from "./modals/SettingsModal";
 
 const UserInterface = () => {
   const [expanded, setExpanded] = useState(false);
@@ -15,15 +14,21 @@ const UserInterface = () => {
   const [openInstructionModal, setOpenInstructionModal] = useState(false);
   const [openSettingsModal, setOpenSettingsModal] = useState(false);
 
+  function getStyles(name) {
+    return getComputedStyle(document.documentElement).getPropertyValue(name);
+  }
 
   const animationProps = useSpring({
-    height: expanded ? "520px" : "120px",
+    height: expanded
+      ? `${getStyles("--animationProps_height_true")}`
+      : `${getStyles("--animationProps_height_false")}`,
     config: { duration: 150 },
   });
 
   const buttonAnimationProps = useSpring({
     opacity: expanded ? 1 : 0,
     pointerEvents: expanded ? "auto" : "none",
+
     config: { tension: 400, friction: 20 },
   });
 
@@ -32,47 +37,49 @@ const UserInterface = () => {
       <animated.div className="user-interface" style={animationProps}>
         <animated.button
           className="expandMenu"
-          onClick={() => setExpanded(!expanded)}>
-        </animated.button>
+          onClick={() => setExpanded(!expanded)}
+        ></animated.button>
 
         <animated.button
           className="account"
           style={buttonAnimationProps}
-          onClick={() => setOpenAccountModal(true)} >
-        </animated.button>
+          onClick={() => setOpenAccountModal(true)}
+        ></animated.button>
 
         <animated.button
           className="difficulty"
           style={buttonAnimationProps}
-          onClick={() => setOpenDifficultyModal(true)} >
-        </animated.button>
+          onClick={() => setOpenDifficultyModal(true)}
+        ></animated.button>
 
         <animated.button
           className="shop"
           style={buttonAnimationProps}
-          onClick={() => setOpenShopModal(true)} >
-        </animated.button>
+          onClick={() => setOpenShopModal(true)}
+        ></animated.button>
 
         <animated.button
           className="instruction"
           style={buttonAnimationProps}
-          onClick={() => setOpenInstructionModal(true)} >
-        </animated.button>
+          onClick={() => setOpenInstructionModal(true)}
+        ></animated.button>
 
         <animated.button
           className="settings"
           style={buttonAnimationProps}
-          onClick={() => setOpenSettingsModal(true)} >
-        </animated.button>
+          onClick={() => setOpenSettingsModal(true)}
+        ></animated.button>
       </animated.div>
 
       {openAccountModal && <AccountModal closeModal={setOpenAccountModal} />}
-      {openDifficultyModal && <DifficultyModal closeModal={setOpenDifficultyModal} />}
+      {openDifficultyModal && (
+        <DifficultyModal closeModal={setOpenDifficultyModal} />
+      )}
       {openShopModal && <ShopModal closeModal={setOpenShopModal} />}
-      {openInstructionModal && <InstructionModal closeModal={setOpenInstructionModal} />}
+      {openInstructionModal && (
+        <InstructionModal closeModal={setOpenInstructionModal} />
+      )}
       {openSettingsModal && <SettingsModal closeModal={setOpenSettingsModal} />}
-
-
     </div>
   );
 };
