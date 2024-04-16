a707e404b0848528235c79cd66ad8cee349648ee
Patryk Kot <kot22886@stud.akademiabialska.pl>
2024-04-16 18:54:45 +0200

    Add BossFightUi animations

--- src/components/interactive-game-ui/BossFightUI.jsx
+++ src/components/interactive-game-ui/BossFightUI.jsx
@@ -1,16 +1,31 @@
-const UiBossFight = () => {
+import React, { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
 
+const UiBossFight = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  const animationProps = useSpring({
+    transform: isVisible ? "translateY(0%)" : "translateY(120%)",
+  });
+  const questionAnimationProps= useSpring({
+    transform: isVisible ? "translateY(0%)" : "translateY(-120%)",
+  });
 
   return (
-  <div className="BossFightContainer"style={{justifyContent:"center"}}>
-    <button className="Question">Question</button>
-    <div className="Answers">
-     <button className="Answer1">Answer1</button>
-     <button className="Answer2">Answer2</button>   
-     <button className="Answer3">Answer3</button>      
-     <button className="Answer4">Answer4</button>   
+    <div className="BossFightContainer" style={{ justifyContent: "center" }}>
+      <animated.button className="Question"style={questionAnimationProps}>Question</animated.button>
+      <animated.div className="Answers" style={animationProps}>
+        <button className="Answer1">Answer1</button>
+        <button className="Answer2">Answer2</button>
+        <button className="Answer3">Answer3</button>
+        <button className="Answer4">Answer4</button>
+      </animated.div>
     </div>
-    </div>
-);
+  );
 };
+
 export default UiBossFight;
