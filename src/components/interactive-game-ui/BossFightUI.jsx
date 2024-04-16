3c32d2d13bbb42149c9ff884cf27c04896fdc615
Bartosz Arseniuk <bartekarseniuk1701@gmail.com>
2024-04-16 20:41:55 +0200

    Add timer

--- src/components/interactive-game-ui/BossFightUI.jsx
+++ src/components/interactive-game-ui/BossFightUI.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
 const UiBossFight = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -11,19 +12,37 @@ const UiBossFight = () => {
   const animationProps = useSpring({
     transform: isVisible ? "translateY(0%)" : "translateY(120%)",
   });
+
   const questionAnimationProps= useSpring({
     transform: isVisible ? "translateY(0%)" : "translateY(-120%)",
   });
 
   return (
     <div className="BossFightContainer" style={{ justifyContent: "center" }}>
-      <animated.button className="Question"style={questionAnimationProps}>Question</animated.button>
+      <animated.button className="Question" style={questionAnimationProps}>Question</animated.button>
       <animated.div className="Answers" style={animationProps}>
         <button className="Answer1">Answer1</button>
         <button className="Answer2">Answer2</button>
         <button className="Answer3">Answer3</button>
         <button className="Answer4">Answer4</button>
       </animated.div>
+      <animated.div className="clock-bg">
+      <div className="clock-container">
+      <CountdownCircleTimer
+        isPlaying
+        duration={60}
+        colors={[
+          '#004777',
+        ]}
+      >
+        {({ remainingTime }) => (
+          <div className="time">
+            {remainingTime}
+          </div>
+        )}
+      </CountdownCircleTimer>
+    </div>
+    </animated.div>
     </div>
   );
 };
