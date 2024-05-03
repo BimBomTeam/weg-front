3f0698d04efc86dbd85fc73fc5ad5c167651f8c1
Bartosz Arseniuk <bartekarseniuk1701@gmail.com>
2024-05-03 14:25:59 +0200

    Fix dots animations

--- src/components/user-interface/user-interface/hints/Loader.jsx
+++ src/components/user-interface/user-interface/hints/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="bouncing-dots">
+      <div className="loading-dots">
         <div className="dot"></div>
         <div className="dot"></div>
         <div className="dot"></div>
